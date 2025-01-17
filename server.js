0ad239c7fc88ba75948804f9baa6935564485664
Marc Brown <133875650+mbrown007@users.noreply.github.com>
2025-01-17 19:36:24 +0000

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,11 @@
  * It uses in-memory data and randomized metrics, 
  * allowing you to experiment with how an AWS Connect 
  * client might interact with a real AWS Connect service.
+ * Set error rate:
+ * curl -X POST http://localhost:3000/admin/errorRate \ -H 'Content-Type: application/json' \ -d '{"rate": 1}'
+ * set random error rate 10%:
+ * curl -X POST http://localhost:3000/admin/errorRate \ -H 'Content-Type: application/json' \ -d '{"rate": 1}'
+
  ****************************************************/
 
 const express = require('express');     // Express framework for routing and HTTP handling
